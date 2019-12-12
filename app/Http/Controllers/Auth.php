42ccd18197e362a32860856c713f5c5642de5174
Space <webproxa@gmail.com>
2019-12-12 11:54:24 +0300

    add signup & signin

---
+++ app/Http/Controllers/Auth.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class Auth extends Controller
+{
+    public function login(Request $request)
+    {
+        $rules = [
+            'email' => 'required',
+            'password' => 'required'
+        ];
+
+        $m = [
+            'required'=> 'req :attribute'
+        ];
+
+        $v = Validator::make(
+                $request->all(),
+                $rules,
+                $m
+            );
+
+
+
+        if ($v->fails()) {
+            return response()
+                    ->json($v->messages(), 422);
+        }
+
+
+        $user = User::where('email', $request->email)->first();
+
+        $check = Hash::check(
+            $request->password,
+            $user->password);
+
+        if ($check) {
+            $user->api_token = Str::random(80);
+            $user->save();
+
+            return response()
+                    ->json(['token'=> $user->api_token], 200);
+        } else {
+            return response()
+                    ->json(['login'=> 'incorrect login or password'], 404);
+        }
+    }
+
+
+    public function signup(Request $request)
+    {
+        $rules = [
+            'email' => 'required|email|unique:users',
+            'name' => 'required|string|max:50',
+            'password' => 'required'
+        ];
+
+        $m = [
+            'required'=> 'req :attribute'
+        ];
+
+        $v = Validator::make(
+                $request->all(),
+                $rules,
+                $m
+            );
+
+
+
+        if ($v->fails()) {
+            return response()
+                    ->json($v->messages(), 422);
+        }
+
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        return response()->json([
+            'id'=> $user->id
+        ], 201);
+    }
+}
---
+++ app/Http/Middleware/ApiAuth.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // return response('err', 401);
+        return $next($request);
+    }
+}
